3361386a90e0f30938d7faf601289a7fd20860d8
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-04-13 18:37:28 +1000

    переделанное ДЗ, после исправления

--- src/main/java/pro/sky/java/course2/homework24/CalculatorService.java
+++ src/main/java/pro/sky/java/course2/homework24/CalculatorService.java
@@ -1,121 +1,145 @@
 package pro.sky.java.course2.homework24;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
 
-    public static String getResult(String args) {
-        Integer num1 = CalculatorService.getNum1(args);
-        int num2 = CalculatorService.getNum2(args);
-        String mathSign = CalculatorService.getMathSign(args);
-
-        if(num2 == 0 && mathSign.equals("/")){
-            return "деление на ноль невозможно";
+    public int plus(Integer num1, Integer num2) {
+        if (num1.equals(null) || num2.equals(null)){
+            throw new IllegalArgumentException("не хватает аргумента для вычисления");
         }
-
-        int result = 0;
-        switch (mathSign){
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                result = num1 / num2;
-                break;
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-         }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(num1 + "" + mathSign + "" + num2 + " = " + result);
-        return "<strong>Результат</strong>" + "<br><strong>" + sb + "</strong></br>";
+        return num1 + num2;
     }
 
-    public static int getNum1(String args) {
-        char[] symbolsArr = args.toCharArray();
-        StringBuilder sb = new StringBuilder(args);
-        int counter = counterCalc(symbolsArr, args);
-        sb.delete(0, symbolsArr.length);
-        for (int i = 0; i < counter; i++) {
-            switch (symbolsArr[i]) {
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    sb.append(symbolsArr[i]);
-                    break;
-            }
-        }
-        return Integer.parseInt(String.valueOf(sb));
+    public int minus(int num1, int num2) {
+        return num1 - num2;
     }
 
-    public static String getMathSign(String args) {
-        char[] symbolsArr = args.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < symbolsArr.length; i++) {
-            switch (symbolsArr[i]) {
-                case '*':
-                case '/':
-                case '+':
-                case '-':
-                    sb.append(symbolsArr[i]);
-                    break;
-            }
-        }
-        return String.valueOf(sb);
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
     }
 
-    public static int getNum2(String args) {
-        char[] symbolsArr = args.toCharArray();
-        StringBuilder sb = new StringBuilder(args);
-
-        int counter = counterCalc(symbolsArr, args);
-
-        sb.delete(0, symbolsArr.length);
-        for (int i = counter; i < symbolsArr.length; i++) {
-            switch (symbolsArr[i]) {
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    sb.append(symbolsArr[i]);
-                    break;
-            }
+    public double separate(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Деление на 0 невозможно");
         }
-        return Integer.parseInt(String.valueOf(sb));
+        return (double) num1 / num2;
     }
-    public static int counterCalc(char[] Arr, String args){
-        StringBuilder sb = new StringBuilder(args);
-        int counter = 0;
-        for(char el: Arr){
-            switch (el){
-                case'*':
-                    counter = sb.indexOf("*");
-                    break;
-                case'/':
-                    counter = sb.indexOf("/");
-                    break;
-                case'+':
-                    counter = sb.indexOf("+");
-                    break;
-                case'-':
-                    counter = sb.indexOf("-");
-                    break;
-            }
-        }
-        return counter;
-    }
+//    public String getResult(String args) {
+//        Integer num1 = getNum1(args);
+//        int num2 = getNum2(args);
+//        String mathSign = getMathSign(args);
+//
+//        if(num2 == 0 && mathSign.equals("/")){
+//            return "деление на ноль невозможно";
+//        }
+//
+//        int result = 0;
+//        switch (mathSign){
+//            case "*":
+//                result = num1 * num2;
+//                break;
+//            case "/":
+//                result = num1 / num2;
+//                break;
+//            case "+":
+//                result = num1 + num2;
+//                break;
+//            case "-":
+//                result = num1 - num2;
+//                break;
+//         }
+//
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(num1 + "" + mathSign + "" + num2 + " = " + result);
+//        return "<strong>Результат</strong>" + "<br><strong>" + sb + "</strong></br>";
+//    }
+//
+//    public int getNum1(String args) {
+//        char[] symbolsArr = args.toCharArray();
+//        StringBuilder sb = new StringBuilder(args);
+//        int counter = counterCalc(symbolsArr, args);
+//        sb.delete(0, symbolsArr.length);
+//        for (int i = 0; i < counter; i++) {
+//            switch (symbolsArr[i]) {
+//                case '0':
+//                case '1':
+//                case '2':
+//                case '3':
+//                case '4':
+//                case '5':
+//                case '6':
+//                case '7':
+//                case '8':
+//                case '9':
+//                    sb.append(symbolsArr[i]);
+//                    break;
+//            }
+//        }
+//        return Integer.parseInt(String.valueOf(sb));
+//    }
+//
+//    public String getMathSign(String args) {
+//        char[] symbolsArr = args.toCharArray();
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < symbolsArr.length; i++) {
+//            switch (symbolsArr[i]) {
+//                case '*':
+//                case '/':
+//                case '+':
+//                case '-':
+//                    sb.append(symbolsArr[i]);
+//                    break;
+//            }
+//        }
+//        return String.valueOf(sb);
+//    }
+//
+//    public int getNum2(String args) {
+//        char[] symbolsArr = args.toCharArray();
+//        StringBuilder sb = new StringBuilder(args);
+//
+//        int counter = counterCalc(symbolsArr, args);
+//
+//        sb.delete(0, symbolsArr.length);
+//        for (int i = counter; i < symbolsArr.length; i++) {
+//            switch (symbolsArr[i]) {
+//                case '0':
+//                case '1':
+//                case '2':
+//                case '3':
+//                case '4':
+//                case '5':
+//                case '6':
+//                case '7':
+//                case '8':
+//                case '9':
+//                    sb.append(symbolsArr[i]);
+//                    break;
+//            }
+//        }
+//        return Integer.parseInt(String.valueOf(sb));
+//    }
+//    public int counterCalc(char[] Arr, String args){
+//        StringBuilder sb = new StringBuilder(args);
+//        int counter = 0;
+//        for(char el: Arr){
+//            switch (el){
+//                case'*':
+//                    counter = sb.indexOf("*");
+//                    break;
+//                case'/':
+//                    counter = sb.indexOf("/");
+//                    break;
+//                case'+':
+//                    counter = sb.indexOf("+");
+//                    break;
+//                case'-':
+//                    counter = sb.indexOf("-");
+//                    break;
+//            }
+//        }
+//        return counter;
+//    }
 }
--- src/main/java/pro/sky/java/course2/homework24/controller/Controller.java
+++ src/main/java/pro/sky/java/course2/homework24/controller/Controller.java
@@ -1,20 +1,33 @@
 package pro.sky.java.course2.homework24.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.java.course2.homework24.CalculatorService;
 
+import java.text.DecimalFormat;
+
 @RestController
+@RequestMapping("/calculator")
 public class Controller {
-    @GetMapping
-    @ResponseBody
-    public String showSiteName(){
-        return "<strong>Калькулятор</strong>";
+    CalculatorService cs = new CalculatorService();
+
+    @GetMapping()
+    public String calculator(){
+        return "<strong>Добро пожаловать в калькулятор</strong>";
     }
-    @GetMapping("/calc")
-    public String calculator(@RequestParam String args){
-       return CalculatorService.getResult(args);
+    @GetMapping("/plus")
+    public String plus(@RequestParam int num1, int num2){
+        return num1 + " + " + num2 + " = " + cs.plus(num1, num2);
+    }
+    @GetMapping("/minus")
+    public String minus(@RequestParam int num1, int num2){
+        return num1 + " - " + num2 + " = " + cs.minus(num1, num2);
+    }
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int num1, int num2){
+        return num1 + " х " + num2 + " = " + cs.multiply(num1, num2);
+    }
+    @GetMapping("/separate")
+    public String separate(@RequestParam int num1, int num2){
+        return num1 + " / " + num2 + " = " + String.format("%.4f", cs.separate(num1, num2));
     }
 }
